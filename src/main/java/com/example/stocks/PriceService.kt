7911499d988d6abb2d0a542543ea3b9560ef0497
clover <tclover@tutanota.com>
2020-03-03 11:23:21 +0100

    fix rsocket stream

--- src/main/java/com/example/stocks/PriceService.kt
+++ src/main/java/com/example/stocks/PriceService.kt
@@ -5,15 +5,21 @@ import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import java.time.Duration
 import java.time.LocalDateTime
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ThreadLocalRandom
 
 @Service
 class PriceService {
+    private val prices = ConcurrentHashMap<String, Flux<StockPrice>>()
+
     fun getPrices(symbol: String): Flux<StockPrice> {
-        return Flux.interval(Duration.ofSeconds(1))
-                .map { StockPrice(symbol, generateRandomPrice(), LocalDateTime.now()) }
-                .share()
+        return prices.computeIfAbsent(symbol) {
+            Flux.interval(Duration.ofSeconds(1))
+                    .map { StockPrice(symbol, generateRandomPrice(), LocalDateTime.now()) }
+                    .share()
+        }
     }
+
     private fun generateRandomPrice(): Double {
         return ThreadLocalRandom.current().nextDouble(50.0)
     }
